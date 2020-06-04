57b4b134af2e18e674c730472f7e53c500a8fd5b
Yuri Konotopov <ykonotopov@gnome.org>
2020-06-04 22:52:38 +0400

    extension-toggle: change color to primary and add hover effect

--- src/components/ExtensionToggle.vue
+++ src/components/ExtensionToggle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class='extension-toggle'>
         <div :checked="checked" @click="$emit('click', !checked)" class="toggle" :class="{'on' : checked}" ></div>
     </div>
 </template>
@@ -21,36 +21,42 @@
 $extension-toggle-width: 54px;
 $extension-toggle-height: 32px;
 $extension-toggle-circle-width: 8px;
-$extension-toggle-color: #28a745;
+$extension-toggle-color: #4a86cf;
 
-.toggle{
-    cursor: pointer;
-    width: $extension-toggle-width;
-    height: $extension-toggle-height;
-    border-radius: $extension-toggle-height / 2;
-    box-sizing: border-box;
-    border: 2px solid #ccc;
-    position: relative;
-    padding: 2px;
-    transition: border-color .25s, background-color .25s;
-  
-    &:before{
-        content: "";
-        width: $extension-toggle-height - $extension-toggle-circle-width;
-        height: $extension-toggle-height - $extension-toggle-circle-width;
-        border-radius: 50%;
-        display: block;
-        background: #ccc;
-        transition: background-color .25s, background-color .25s, transform .25s;
-    }
-  
-    &.on{
-        border-color: $extension-toggle-color;;
-        background-color: $extension-toggle-color;
+.extension-toggle {
+    .toggle{
+        cursor: pointer;
+        width: $extension-toggle-width;
+        height: $extension-toggle-height;
+        border-radius: $extension-toggle-height / 2;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        position: relative;
+        padding: 2px;
+        transition: border-color .25s, background-color .25s;
+    
         &:before{
-            background: #fff;
-            transform: translateX(($extension-toggle-width - $extension-toggle-circle-width) / 2)
+            content: "";
+            width: $extension-toggle-height - $extension-toggle-circle-width;
+            height: $extension-toggle-height - $extension-toggle-circle-width;
+            border-radius: 50%;
+            display: block;
+            background: #ccc;
+            transition: background-color .25s, background-color .25s, transform .25s;
+        }
+    
+        &.on{
+            border-color: $extension-toggle-color;;
+            background-color: $extension-toggle-color;
+            &:before{
+                background: #fff;
+                transform: translateX(($extension-toggle-width - $extension-toggle-circle-width) / 2)
+            }
         }
     }
+}
+
+.extension-toggle:hover {
+    filter: brightness(125%);
 }
 </style>
