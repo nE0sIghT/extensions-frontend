4b3bc3f2aab6745f1896459ff1ef8914821ba542
Yuri Konotopov <ykonotopov@gnome.org>
2020-06-07 13:27:17 +0400

    search: show placeholder when results are missing

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -6,25 +6,30 @@
             opacity="0.6"
             spinner-variant="primary"
         >
-            <b-pagination-nav
-                :value="page"
-                :number-of-pages="pages"
-                :link-gen="searchLink"
-                use-router
-                align="right"
-                pills
-            ></b-pagination-nav>
-            <extensions-list
-                :extensions="extensions"
-            ></extensions-list>
-            <b-pagination-nav
-                :value="page"
-                :number-of-pages="pages"
-                :link-gen="searchLink"
-                use-router
-                align="right"
-                pills
-            ></b-pagination-nav>
+            <div v-if="count">
+                <b-pagination-nav
+                    :value="page"
+                    :number-of-pages="pages"
+                    :link-gen="searchLink"
+                    use-router
+                    align="right"
+                    pills
+                ></b-pagination-nav>
+                <extensions-list
+                    :extensions="extensions"
+                ></extensions-list>
+                <b-pagination-nav
+                    :value="page"
+                    :number-of-pages="pages"
+                    :link-gen="searchLink"
+                    use-router
+                    align="right"
+                    pills
+                ></b-pagination-nav>
+            </div>
+            <div v-else class='text-center'>
+                {{ $t("No extensions found while searching “{query}”", { query: $route.params.query }) }}
+            </div>
         </b-overlay>
     </b-container>    
 </template>
